74bb66c71974099cffb9f2c47a450d9dee8a8f94
Alejandro David Benolol <alejandro.david.benolol@gmail.com>
2022-07-26 18:57:22 +0300

    Agrego un pequeño proyecto para cambiar el tema de una pagina de oscuro a claro con vanilla js

---
+++ DarkAndLightMode/script.js
@@ -0,0 +1,17 @@
+const preferedColorScheme = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+).matches ? 'dark' : 'light';
+//console.log(preferedColorScheme)
+const slider = document.getElementById('slider');
+
+const setThheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+slider.addEventListener('click', () => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setThheme(switchToTheme);
+})
+
+setThheme(localStorage.getItem('theme') || preferedColorScheme);
